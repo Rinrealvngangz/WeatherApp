c3a03e4795000c50ff2a1bd3628e62e817513e91
Rinrealvngangz <tuannmctk42@gmail.com>
2020-10-20 21:43:55 +0700

    add second design

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.weatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.android.volley.toolbox.Volley;
 
@@ -10,16 +13,29 @@ public class MainActivity extends AppCompatActivity {
 
      private  weather _weather;
      private  String name,temp;
-
+     private LinearLayout linearLayoutHori,linearLayoutVerti;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       _weather = weather.getInstance();
-       _weather.weather(temp,name);
-      String Name = _weather.getName();
-      String Temp =  _weather.getTemp();
-    }
+        initId();
+        LayoutInflater layoutInflaterItemsHourLy = LayoutInflater.from(this);
+        LayoutInflater layoutInflaterItemsDaily = LayoutInflater.from(this);
 
+
+        for(int i =0;i<24;i++){
+            View view = layoutInflaterItemsHourLy.inflate(R.layout.item,linearLayoutHori,false);
+            linearLayoutHori.addView(view);
+        }
+        for(int i=0;i<10;i++){
+            View view =layoutInflaterItemsDaily.inflate(R.layout.itemdaily,linearLayoutVerti,false);
+            linearLayoutVerti.addView(view);
+
+        }
+    }
+     private void initId(){
+         linearLayoutHori =findViewById(R.id.itemHourLy);
+         linearLayoutVerti =findViewById(R.id.itemsDaily);
+     }
 }
